5343665e3e4cc0a56fb611ec0bb98be757b7e165
Siddharth Singh <sidsingh264@gmail.com>
2023-09-30 10:11:33 +0530

    mongodb-integrated

--- task-manager/starter/app.js
+++ task-manager/starter/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
-
+const connectDB = require('./db/connect')
+require('dotenv').config()
 //middleware
 
 app.use(express.json())
-
-
 
 //routes
 app.get('/hello', (req, res) => {
@@ -17,4 +16,13 @@ app.use('/api/v1/tasks',tasks)
 
 const port = 3000
 
-app.listen(port, console.log(`Server is running on port ${port}`))
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is running on port ${port}`))
+    }catch(error){
+        console.log(error)
+    }
+}
+
+start()
--- task-manager/starter/controllers/tasks.js
+++ task-manager/starter/controllers/tasks.js
@@ -1,13 +1,18 @@
+const Task = require('../models/Task');
+
+
 const getAllTasks = (req,res) => {
     res.send('all items from cc')
 }
 
-const createTask = (req,res) => {
-    res.send('create Task')
+const createTask = async (req,res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+    //res.json(req.body)
 }
 
 const getTask = (req,res) => {
-    res.send('get single Task')
+    res.json({id:req.params.id})
 }
 
 const updateTask = (req,res) => {
