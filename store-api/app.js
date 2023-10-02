bc1109333d0df87eee76334903bf511295b52b72
Siddharth Singh <sidsingh264@gmail.com>
2023-10-03 00:56:35 +0530

    stori api mongo db connected

--- store-api/app.js
+++ store-api/app.js
@@ -1,8 +1,14 @@
 require('dotenv').config()
+require('express-async-errors')
 
 
 const express = require('express');
 const app = express();
+
+
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler')
@@ -14,8 +20,10 @@ app.use(express.json())
 //routes
 
 app.get('/',(req, res)=>{
-    res.send('<h1>Store API</h1> <a href="/api/v1/products"></a>')
+    res.send('<h1>Store API</h1> <a href="/api/v1/products">products route</a>')
 })
+
+app.use('/api/v1/products',productsRouter)
 
 //products route
 
@@ -27,7 +35,7 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
-        //connectDB
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening on port ${port}`))
     } catch (error) {
         console.log(error)
---
+++ store-api/controllers/products.js
@@ -0,0 +1,28 @@
+const Product = require('../models/product')
+
+
+const getAllProductsStatic = async(req, res) => {
+    const products = await Product.find({featured: true})
+    res.status(200).json({products,  nbHits: products.length})
+}
+
+const getAllProducts = async(req, res) => {
+    const {featured} = req.query
+
+    const queryObject = {}
+
+    if(featured){
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
+    res.status(200).json({products, nbHits: products.length})
+
+
+    //console.log(req.query)
+}
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic
+}
