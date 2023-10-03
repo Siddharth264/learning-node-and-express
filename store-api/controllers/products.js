dd7b931c4565f54586e095ec2426e0b9e601e749
Siddharth Singh <sidsingh264@gmail.com>
2023-10-03 18:10:52 +0530

    sorting and filtering added to store api

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,21 +1,45 @@
 const Product = require('../models/product')
 
-
 const getAllProductsStatic = async(req, res) => {
-    const products = await Product.find({featured: true})
+    const products = await Product.find({}).sort('name').select('name price').limit(10).skip(5)
     res.status(200).json({products,  nbHits: products.length})
 }
 
 const getAllProducts = async(req, res) => {
-    const {featured} = req.query
+    const {featured, company, name, sort, fields} = req.query
 
     const queryObject = {}
 
     if(featured){
         queryObject.featured = featured === 'true' ? true : false
     }
-    console.log(queryObject)
-    const products = await Product.find(queryObject)
+    if(company){
+        queryObject.company = company
+    }
+    if(name){
+        queryObject.name = { $regex: name, $options: 'i'}
+    }
+    //console.log(queryObject)
+    let result =  Product.find(queryObject)
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList)
+        //products = products.sort()
+    }else{
+        result = result.sort('createdAt')
+    }
+
+    if(fields){
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+
+    const skip = (page-1)*limit
+    result = result.skip(skip).limit(limit)
+    const products = await result
     res.status(200).json({products, nbHits: products.length})
 
 
