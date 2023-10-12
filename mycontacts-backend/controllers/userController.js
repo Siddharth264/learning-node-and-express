66a5130119b4b5b0ade13410b07200ef2540ee0d
Siddharth Singh <sidsingh264@gmail.com>
2023-10-12 23:00:32 +0530

    user authentication added in mycontacts

---
+++ mycontacts-backend/controllers/userController.js
@@ -0,0 +1,84 @@
+const expressAsyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+//@description Register a user
+//@route POST /api/users/register
+//@access Public
+
+const registerUser = expressAsyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const userAvailable = await User.findOne({ email });
+
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User is already registered");
+  }
+
+  //HashPassword
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  console.log(`User created ${user}`);
+
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data not valid");
+  }
+});
+
+//@description Login user
+//@route POST /api/users/login
+//@access Public
+
+const loginUser = expressAsyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
+
+  const user = await User.findOne({ email });
+
+  //compare password with hashed password
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign({
+      user: {
+        username: user.username,
+        email: user.email,
+        id: user.id,
+      },
+    }, process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "1m"}
+    );
+    res.status(200).json({ accessToken });
+  }else{
+    res.status(401)
+    throw new Error("email or password is not valid")
+  }
+});
+
+//@description Current user Information
+//@route POST /api/users/current
+//@access private
+
+const currentUser = expressAsyncHandler(async (req, res) => {
+  res.json(req.user);
+});
+
+module.exports = { registerUser, loginUser, currentUser };
--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const routes = require('./routes/contactRoutes')
+const contactRoutes = require('./routes/contactRoutes')
+const userRoutes = require('./routes/userRoutes')
 const errorHandler = require('./middleware/errorHandler')
 
 const connectDb = require('./config/dbConnection')
@@ -12,7 +13,8 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
-app.use("/api/contacts",routes)
+app.use("/api/contacts",contactRoutes)
+app.use("/api/users",userRoutes)
 app.use(errorHandler)
 
 app.listen(port,()=>{
